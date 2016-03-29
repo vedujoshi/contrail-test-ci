Juniper/contrail-test

Add support to run tests in a user-specified single tenant
Currently each test suite creates its own projects/users
In production test environments, the admin would allocate
a specific project where the tests are expected to run
(stackUser, stackPassword and stackTenant in sanity_params.ini)

If tenant_isolation is enabled and admin credentials are
mentioned in sanity_params.ini, the existing behavior will
be seen

Corresponding fab code changes : https://review.opencontrail.org/16483

Change-Id: I13e5d0983472b1a8f9d2ecdd6f79e5df043ee04b

common/openstack_libs.py
@@ -59,3 +59,8 @@ try:
 except:
     ceilo_client = None
 
+try:
+    from glanceclient import Client as GlanceClient
+except:
+    glance_client = None
+

vedujoshi/contrail-test-ci

Conflicts:
	common/isolated_creds.py
	fixtures/nova_test.py
	scripts/analytics/base.py
	scripts/ceilometer_tests/base.py
	scripts/ecmp/base.py
	scripts/encap/base.py
	scripts/heat/base.py
	scripts/multi_tenancy/base.py
	scripts/policy/base.py
	scripts/rt_filter/base.py
	scripts/smgr/base.py
	scripts/svc_firewall/base.py
	scripts/svc_mirror/base.py
	scripts/vpc/base.py
	serial_scripts/analytics/base.py
	serial_scripts/control_node_scaling/base.py
	serial_scripts/discovery_regression/base.py
	serial_scripts/ecmp/base.py
	serial_scripts/encap/base.py
	serial_scripts/evpn/base.py
	serial_scripts/floatingip/base.py
	serial_scripts/flow_tests/base.py
	serial_scripts/ha/base.py
	serial_scripts/headless_vrouter/base.py
	serial_scripts/md5/base.py
	serial_scripts/policy/base.py
	serial_scripts/rsyslog/base.py
	serial_scripts/system_test/flow_tests/base.py
	serial_scripts/upgrade/base.py
	serial_scripts/vgw/base.py
	serial_scripts/vm_regression/base.py
	serial_scripts/webui/base.py

common/openstack_libs.py
@@ -59,3 +59,8 @@ try:
 except:
     ceilo_client = None
 
+try:
+    from glanceclient import Client as glance_client
+except:
+    glance_client = None
+